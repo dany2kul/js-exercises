e755228617d9c72a61cdaea0f35fa6f58303707f
tchapda daniella <dany2kul@yahoo.com>
2018-03-17 12:22:27 +0000

    javasript week 2 and 3A

--- week-2-practise/level-1/2-validation.js
+++ week-2-practise/level-1/2-validation.js
@@ -2,9 +2,9 @@
 // TIP: You should write an expression that returns a boolean value
 
 var num = 10;
-var isBigEnough; // ONLY EDIT THIS LINE
+var isBigEnough
 
-if (isBigEnough) {
+if (!isBigEnough) {
   console.log("num is bigger than or equal to 10");
 } else {
   console.log("num is not big enough");
